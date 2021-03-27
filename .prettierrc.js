4a6344f5a9bce2242139671d70a964b28245d275
lumengxin <2735146963@qq.com>
2021-03-27 21:56:11 +0800

    '清空master'

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    semi: false,    // 不加;
-    singleQuote: true,  // 使用''
-    printWidth: 100,    // 每行最长100字符换行
-}
--- example/index.md
+++
@@ -1,6 +0,0 @@
----
-title: '引导'
-author: 'xin'
----
-
-## example
