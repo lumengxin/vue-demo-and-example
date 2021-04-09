8fda9f9977455798550451f60d80f4016158b59f
lumengxin <2735146963@qq.com>
2021-04-09 23:04:50 +0800

    feat(视图层): 首页welcome；测试commit图标

--- .cz-config.js
+++ .cz-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   // 修改性质
   types: [
-    { value: 'feat', name: 'feat: 新功能' },
-    { value: 'fix', name: 'fix: 修复bug' },
-    { value: 'docs', name: 'docs: 文档修改' },
-    { value: 'style', name: 'style: 代码格式（空格、分号等）' },
-    { value: 'refactor', name: 'refactor: 重构（非feat、非fix）' },
-    { value: 'ci', name: 'ci: 持续集成的配置文件或脚本的修改' },
-    { value: 'perf', name: 'perf: 提高性能' },
-    { value: 'test', name: 'test: 添加缺少的测试' },
-    { value: 'build', name: 'build: 构建工具或外部依赖包的修改' },
-    { value: 'WIP', name: 'WIP: 进行中的工作' },
+    { value: 'feat', name: ' 🍓 feat: 新功能' },
+    { value: 'fix', name: ' 🏓 fix: 修复bug' },
+    { value: 'docs', name: ' 📦 docs: 文档修改' },
+    { value: 'style', name: ' 👨🏻‍💻 style: 代码格式（空格、分号等）' },
+    { value: 'refactor', name: ' 🏗 refactor: 重构（非feat、非fix）' },
+    { value: 'ci', name: ' 👀 ci: 持续集成的配置文件或脚本的修改' },
+    { value: 'perf', name: ' 🧪 perf: 提高性能' },
+    { value: 'test', name: ' 🦄 test: 添加缺少的测试' },
+    { value: 'build', name: ' 🐛 build: 构建工具或外部依赖包的修改' },
+    { value: 'WIP', name: ' 👨🏻‍🚀 WIP: 进行中的工作' },
     {
       value: 'chore',
-      name: 'chore: 杂项。其他不修改源代码与测试代码的修改',
+      name: ' 🐢 chore: 杂项。其他不修改源代码与测试代码的修改',
     },
-    { value: 'revert', name: 'revert: 撤销某次提交' },
+    { value: 'revert', name: ' 🌚 revert: 撤销某次提交' },
   ],
   // 修改范围
   scopes: [{ name: '数据层' }, { name: '控制层' }, { name: '视图层' }],
@@ -31,5 +31,5 @@ module.exports = {
   // Body: 对 subject 的补充。可以多行。Footer: 主要是一些关联 issue 的操作。
   skipQuestions: ['body', 'footer'],
   // 描述的长度限制
-  subjectLimit: 100, 
+  subjectLimit: 100,
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,79 @@
 <template>
-  <div class="container"></div>
+  <div class="index">
+    <div class="welcome">
+      <h1>记录前端开发中的一些实用知识</h1>
+      <p>没有固定形式，想到哪做到哪，到处好的网站抄一点样式布局。</p>
+      <p>
+        整理一些知识文档，以实际例子形式记录工作日常开发中一些亮点，或者一些常规及非常规处理方案。
+      </p>
+      <div class="topic">
+        <div v-for="(item, index) in topics" :key="index" class="topic-card">
+          <div class="icon">
+            <i :class="item.icon"></i>
+          </div>
+          <div class="title">
+            <h2>{{ item.title }}</h2>
+            <p>{{ item.content }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // components根目录vue可以直接使用，无需注册
 export default {
   name: 'Index',
+  data() {
+    return {
+      topics: [
+        {
+          icon: 'aaa',
+          title: '问答',
+          content: '提出问题并向他人学习',
+        },
+        {
+          icon: 'aaa',
+          title: '问答',
+          content: '提出问题并向他人学习',
+        },
+        {
+          icon: 'aaa',
+          title: '问答',
+          content: '提出问题并向他人学习',
+        },
+        {
+          icon: 'aaa',
+          title: '问答',
+          content: '提出问题并向他人学习',
+        },
+      ],
+    }
+  },
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="stylus" scoped>
+.index
+  padding-top 3.75rem
+  .welcome
+    text-align center
+    color #fff
+    background no-repeat 50% url(static/welcome-bg.png)
+  .topic
+    position relative
+    top 3.75rem
+    display flex
+    justify-content space-between
+    align-items center
+    min-width 58.75rem
+    width 70rem
+    margin 0 auto
+    .topic-card
+      border 1px solid darkgrey
+      background #fff
+      color #000
+      border-radius .25rem
+      box-shadow .0625rem .0625rem .0625rem darkgrey
 </style>
